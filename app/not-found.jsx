608115ef2fc47583e97b37c7579a566b758391f3
HugoCoder1 <hugueshugues2002@gmail.com>
2026-01-31 09:25:41 +0100

    feat: ajout de la page 404

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Example() {
+export default function NotFoundPage() {
   return (
     <div className="flex justify-center min-h-screen m-auto">
       <div className="flex flex-col items-center justify-center text-sm max-md:px-4">
